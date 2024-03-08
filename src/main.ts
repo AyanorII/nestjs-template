ee9daa27d3ad9ad65eab4bda0c7c18600312369d
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2024-03-08 10:50:25 +0900

    feat: set swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Logger, ValidationPipe } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { Config } from "config/configuration";
 
 import { AppModule } from "./app.module";
@@ -11,6 +12,13 @@ async function bootstrap() {
 	const logger = new Logger();
 
 	app.useGlobalPipes(new ValidationPipe());
+
+	const swaggerConfig = new DocumentBuilder()
+		.setTitle("NestJS API")
+		.setDescription("NestJS API description")
+		.build();
+	const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+	SwaggerModule.setup("docs", app, swaggerDocument);
 
 	const port = configService.get<Config["PORT"]>("PORT") || 8000;
 	await app.listen(port);
